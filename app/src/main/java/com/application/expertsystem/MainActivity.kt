46f7f1bc0f131ade397c10e15443f4bb816938fc
Калистый Никита <prostonikita8848@gmail.com>
2022-04-01 13:44:08 +0200

    create markup

--- app/src/main/java/com/application/expertsystem/MainActivity.kt
+++ app/src/main/java/com/application/expertsystem/MainActivity.kt
@@ -2,14 +2,40 @@ package com.application.expertsystem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 import com.application.expertsystem.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    /*lateinit var binding: ActivityMainBinding*/
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        /*binding = ActivityMainBinding.inflate(layoutInflater)*/
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.etOldCount.addTextChangedListener {
+            binding.llDemandCountContainer.removeAllViews()
+
+            //null safety надо допилить
+            val count = binding.etOldCount.getText().toString()
+            val countInt:Int? = count?.toInt()
+            if (countInt != null && countInt > 0) {
+                for (i in 1..countInt) {
+                    val newParent = layoutInflater.inflate(
+                        R.layout.item_demand_element,
+                        this.binding.llDemandCountContainer,
+                        false
+                    )
+                    newParent.id = i
+                    newParent.apply {
+                        this.id = i
+                        val parentNumber = this.findViewById<TextView>(R.id.tv_parent)
+                        parentNumber.text = "Родитель ${i}"
+                    }
+                    this.binding.llDemandCountContainer.addView(newParent)
+                }
+            }
+        }
+        setContentView(binding.root)
     }
 }
