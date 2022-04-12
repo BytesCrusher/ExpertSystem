ac12532fcb93f561a784cb6c6cedb0203535de6e
Калистый Никита <prostonikita8848@gmail.com>
2022-04-12 17:11:03 +0200

    add vidgets listners

--- app/src/main/java/com/application/expertsystem/MainActivity.kt
+++ app/src/main/java/com/application/expertsystem/MainActivity.kt
@@ -2,9 +2,11 @@ package com.application.expertsystem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import androidx.core.widget.addTextChangedListener
 import com.application.expertsystem.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,26 +15,55 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
+        /*binding.tvMainText.text = "[${binding.etOldCount.text}]"*/
+
+        var parentDemandList = mutableMapOf<Int, View>()
+
         binding.etOldCount.addTextChangedListener {
             binding.llDemandCountContainer.removeAllViews()
+            parentDemandList = mutableMapOf<Int, View>()//обнуление мапы с зарплатами родителей
+            binding.tvMainText.text = ""//обнуление вьюшки с текстом
 
-            //null safety надо допилить
-            val count = binding.etOldCount.getText().toString()
-            val countInt:Int? = count?.toInt()
-            if (countInt != null && countInt > 0) {
-                for (i in 1..countInt) {
-                    val newParent = layoutInflater.inflate(
-                        R.layout.item_demand_element,
-                        this.binding.llDemandCountContainer,
-                        false
-                    )
-                    newParent.id = i
-                    newParent.apply {
-                        this.id = i
-                        val parentNumber = this.findViewById<TextView>(R.id.tv_parent)
-                        parentNumber.text = "Родитель ${i}"
+            val count = binding.etOldCount.text.toString()
+            if (count != "") {
+                val countInt:Int? = count.toIntOrNull()
+                if (countInt != null){
+                    if (countInt > 0) {
+
+                        for (i in 1..countInt) {
+                            val newParent = layoutInflater.inflate(
+                                R.layout.item_demand_element,
+                                this.binding.llDemandCountContainer,
+                                false
+                            )
+                            newParent.id = i
+                            newParent.apply {
+                                this.id = i
+                                val parentNumber = this.findViewById<TextView>(R.id.tv_parent)
+                                parentNumber.text = "Родитель ${i}"
+                            }
+                            parentDemandList.put(i, newParent)
+                            this.binding.llDemandCountContainer.addView(newParent)
+                        }
                     }
-                    this.binding.llDemandCountContainer.addView(newParent)
+                    /*binding.tvMainText.text = "${parentDemandList.count()}"*///"[${binding.etOldCount.text}]"
+
+                    //обработка ввода зарплаты родителей
+                    for ((key, value) in parentDemandList) {
+                        val parentDemandCertainItem = value.findViewById<TextInputEditText>(R.id.et_parentDemand)
+                        /*parentDemandCertainItem.setBackgroundColor(getResources().getColor(R.color.purple_500))*/
+                        parentDemandCertainItem.addTextChangedListener{
+                            binding.tvMainText.text = binding.tvMainText.text.toString() + parentDemandCertainItem.text.toString()
+
+                        }
+
+                        binding.tvMainText.text = binding.tvMainText.text.toString() + value.id
+                    }
+
+                    /*binding.tvMainText.text. = "${parentDemandList.forEach{(key, value) ->
+                        key
+
+                    }}"*/
                 }
             }
         }
